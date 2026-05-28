7f61254e927c0422c7ad5f438ed0e2e9338c2955
Pedro Magno <phmagnolopes@gmail.com>
2026-05-28 17:21:59 -0300

    feat(auth): implementar rota de login e gerador de token JWT no backend

---
+++ core/security.py
@@ -0,0 +1,24 @@
+import jwt
+from datetime import datetime, timedelta
+from passlib.context import CryptContext
+
+secret_key = "chave-secreta-super-segura"  
+algorithm = "HS256"
+token_expiration_minutes = 60
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verificar_senha(senha, senha_hash):
+    return pwd_context.verify(senha, senha_hash)
+
+def gerar_hash_senha(senha):
+    return pwd_context.hash(senha)
+
+def criar_token_jwt(dados: dict):
+
+    dados_copia = dados.copy()
+    expiracao = datetime.utcnow() + timedelta(minutes=token_expiration_minutes)
+    dados_copia.update({"exp": expiracao})
+    token = jwt.encode(dados_copia, secret_key, algorithm=algorithm)
+
+    return token
